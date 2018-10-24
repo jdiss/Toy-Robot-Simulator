6e8b0671fbc27811c5fb88dab8ce5ac869e1cb55
Janaka Dissanayake <janaka.dissan@gmail.com>
2018-10-24 23:45:46 +1100

    Add move test

--- spec/robot-spec.js
+++ spec/robot-spec.js
@@ -1,16 +1,28 @@
 import Robot from '../src/components/robot';
 import Table from '../src/components/table'
+import  Directions from '../src/common/constant';
 
 
 describe('robot cretion', () => {
   let robot;
-  
   beforeEach(() => {
       robot = new Robot(new Table(5, 5));
-      robot.place(1,1,'WEST');
   });
 
-  it("Should place  1,1, WEST ", () => {
-    expect(robot.report()).toEqual('1,1,WEST');
+  it("Should  PLACE 0,0,NORTH ", () => {
+    robot.place(0,0,Directions.NORTH);
+    expect(robot.report()).toEqual(`0,0,${Directions.NORTH}`);
   });
+
+  it("Should place  1,4, EAST ", () => {
+    robot.place(1,4,Directions.EAST);
+    expect(robot.report()).toEqual(`1,4,${Directions.EAST}`);
+  });
+
+  it("Should move to   1,2, NORTH ", () => {
+    robot.place(1,1,Directions.NORTH);
+    robot.move();
+    expect(robot.report()).toEqual(`1,2,${Directions.NORTH}`);
+  });
+
 });
